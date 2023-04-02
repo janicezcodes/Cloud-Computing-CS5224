02c4eeea2f109a23e4c0815317aea555add3278f
y.z <52028002+Yufei-Zheng@users.noreply.github.com>
2023-04-02 13:34:10 +0800

    remove venv

--- TalentHunter/deploy.sh
+++ TalentHunter/deploy.sh
@@ -2,11 +2,11 @@
 if [ -d ".venv" ]
 then
     source .venv/bin/activate
-    python3 app.py
+    python app.py
 else
-    python3 -m venv .venv
+    python -m venv .venv
     source .venv/bin/activate
-    python3 -m pip install --upgrade pip
-    pip3 install -r requirements.txt
-    python3 app.py
+    python -m pip install --upgrade pip
+    pip install -r requirements.txt
+    python app.py
 fi
